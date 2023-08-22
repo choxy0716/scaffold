317f47d7a5e64de615e7377ae5facab2b194bf4d
wanglong <wanglong@wanglongdeMacBook-Pro.local>
2023-08-22 18:06:35 +0800

    datasource

--- datasource/src/main/java/com/choxy/datasource/CustomHikariDatasource.java
+++ datasource/src/main/java/com/choxy/datasource/CustomHikariDatasource.java
@@ -1,28 +1,106 @@
 package com.choxy.datasource;
 
+import com.choxy.enums.DatabaseTypeEnum;
 import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 public class CustomHikariDatasource extends AbstractDatasource{
 
+    private DataSource dataSource;
     public void init() {
-        HikariConfig hikariConfig = new HikariConfig();
+
+    }
+
+    public DataSource getDatasourceConnection(DatabaseTypeEnum databaseTypeEnum) {
+        switch (databaseTypeEnum) {
+            case HIVE:
+                dataSource = getHiveDatasource();
+                break;
+            case MYSQL:
+                dataSource = getMysqlDatasource();
+                break;
+            case ORACLE:
+                dataSource = getOracleDatasource();
+                break;
+            case POSTGRESQL:
+                dataSource = getPgDatasource();
+                break;
+            case STARROCKS:
+                dataSource = getStarRocksDatasource();
+                break;
+            case OCEANBASE:
+                dataSource = getOCBDatasource();
+                break;
+            case CLICKHOUSE:
+                dataSource = getClickhouseDatasource();
+                break;
+            case TIDB:
+                dataSource = getTiDBDatasource();
+                break;
+            default:
+                throw new RuntimeException("unSupport database");
+        }
+        return dataSource;
+    }
+
+    private DataSource getMysqlDatasource() {
+        Properties properties = new Properties();
+        properties.setProperty("", "");
+        return getHikariDatasource(properties);
+    }
+
+    private DataSource getOracleDatasource() {
+        Properties properties = new Properties();
+        return getHikariDatasource(properties);
+    }
+
+    private DataSource getHiveDatasource() {
+        Properties properties = new Properties();
+        return getHikariDatasource(properties);
+    }
+
+    private DataSource getPgDatasource() {
+        Properties properties = new Properties();
+        return getHikariDatasource(properties);
+    }
+
+    private DataSource getStarRocksDatasource() {
+        Properties properties = new Properties();
+        return getHikariDatasource(properties);
+    }
+
+    private DataSource getOCBDatasource() {
+        Properties properties = new Properties();
+        return getHikariDatasource(properties);
+    }
+
+    private DataSource getClickhouseDatasource() {
+        Properties properties = new Properties();
+        return getHikariDatasource(properties);
+    }
+
+    private DataSource getTiDBDatasource() {
+        Properties properties = new Properties();
+        return getHikariDatasource(properties);
     }
 
 
-    public Connection getMysqlConnection() {
-        Connection connection;
-        try {
-            Class.forName("");
-            connection = DriverManager.getConnection("", "", "");
-        } catch (ClassNotFoundException | SQLException e) {
-            throw new RuntimeException(e);
+    private DataSource getHikariDatasource(Properties properties) {
+        DataSource dataSource = null;
+        if (properties.isEmpty()) {
+            return null;
         }
+        try {
+            HikariConfig hikariConfig = new HikariConfig(properties);
+            dataSource = new HikariDataSource(hikariConfig);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return dataSource;
 
-        return connection;
     }
 
 
